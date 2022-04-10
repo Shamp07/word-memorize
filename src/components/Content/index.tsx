d97daeed83e8eb0368e26e208711cee397534ae6
Shamp07 <blog.shamp@gmail.com>
2022-04-10 22:18:48 +0900

    feat: add tranistion with react-router

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import { css, Global } from '@emotion/react';
 
 import { ContentPath } from '@constants/route';
 
 const Root = styled.div`
   margin: 22px;
+  width: 100%;
+  height: 100%;
+`;
+
+const animation = css`
+  .fade-enter .inner {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  .fade-enter-active .inner {
+    opacity: 1;
+    transform: translateX(0%);
+  }
+  .fade-exit .inner {
+    opacity: 1;
+    transform: translateX(0%);
+  }
+  .fade-exit-active .inner {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  .fade-enter-active .inner,
+  .fade-exit-active .inner {
+    transition: opacity 500ms, transform 500ms;
+  }
+`;
+
+const RouteWrapper = styled.div`
+  color: white;
+  background: black;
+  height: 100%;
+  width: 100%;
 `;
 
 function Content() {
+  const { pathname } = useLocation();
+
   return (
     <Root>
-      <Routes>
-        <Route path={ContentPath.HOME} element={<div />} />
-        <Route path={ContentPath.WORD} element={<div />} />
-        <Route path={ContentPath.CARD} element={<div />} />
-        <Route path={ContentPath.GAME} element={<div />} />
-        <Route path={ContentPath.TIMER} element={<div />} />
-        <Route path={ContentPath.ANALYTICS} element={<div />} />
-      </Routes>
+      <Global styles={animation} />
+      {Object.values(ContentPath).map((path) => (
+        <CSSTransition
+          in={pathname === path}
+          addEndListener={(node, done) => {
+            node.addEventListener('transitionend', done, false);
+          }}
+          className="fade"
+        >
+          <Route path={path} element={<RouteWrapper className="inner">{path}</RouteWrapper>} />
+        </CSSTransition>
+      ))}
     </Root>
   );
 }
